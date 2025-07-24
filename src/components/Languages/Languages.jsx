f46f96a50352e402b2764621657073d78852097f
Cristina Portilla <macripco1311@gmail.com>
2025-07-23 21:24:14 -0500

    REafactoring language section

--- src/components/Languages/Languages.jsx
+++ src/components/Languages/Languages.jsx
@@ -9,39 +9,40 @@ import DotsProgress from "../DotsProgress/DotsProgress";
 import "../../styles/languages/languages.css";
 
 function Languages() {
-  const { theme } = useContext(ThemeContext);
-  const { lang } = useContext(LanguageContext);
-
   const translation = useTranslation();
   const languages = translation.languages;
 
   const title = languages.title;
-
   const items = languages.items;
 
   return (
     <section id="languages">
-      <div className="languages-div">
-        <div className="title-section">
-          <h2>{title}</h2>
-        </div>
 
-        <div className="languages-content">
-          {items.map((language, index) => (
-            <div className="language-content" key={language.name}>
-              <div className="language-desc-div">
-                <span className="language-title">{language.name}</span>
-                <br />
-                <span className="language-level"> {language.level}</span>
+      <div className="container-section">
+
+        <div className="languages-div">
+
+          <div className="title-section">
+            <h2>{title}</h2>
+          </div>
+
+          <div className="languages-content">
+            {items.map((language, index) => (
+              <div className="language-content" key={language.name}>
+                <div className="language-desc-div">
+                  <span className="language-title">{language.name}</span>
+                  <br />
+                  <span className="language-level"> {language.level}</span>
+                </div>
+                <div className="language-dots-div">
+                  <DotsProgress
+                    maxNumberOfDots={6}
+                    numberOfActiveDots={language.levelNumber}
+                  />
+                </div>
               </div>
-              <div className="language-dots-div">
-                <DotsProgress
-                  maxNumberOfDots={5}
-                  numberOfActiveDots={language.levelNumber}
-                />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </section>
